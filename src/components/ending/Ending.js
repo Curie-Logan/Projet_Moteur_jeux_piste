558acf2fda740dcefc5d15576b211abfa9e9f373
EchoShadow <63055962+EchoShadow@users.noreply.github.com>
2022-02-20 21:04:12 +0100

    modification du logo

--- src/components/ending/Ending.js
+++ src/components/ending/Ending.js
@@ -2,10 +2,12 @@ import React from "react";
 import './Ending.css';
 import ReactDOM from "react-dom";
 import Menu from '../menu/Menu';
+import Map from "../map/Map";
+import Wrapper from "../../wrapper";
 
 class Ending extends React.Component{
 
-    reloadGame(){
+    returnToMenu(){
         //Reset the localStorage
         localStorage.setItem("progression",JSON.stringify(""));
 
@@ -15,12 +17,24 @@ class Ending extends React.Component{
         );
     }
 
+    closeEnding(){
+        let savedProgression = JSON.parse(localStorage.getItem("progression"));
+        const wrapper = new Wrapper(savedProgression["title"]);
+
+        ReactDOM.render(
+            <Map resume={true} wrapper={wrapper} geolocation={true}/>,
+            document.getElementsByClassName("App-header")[0]
+        );
+    }
+
+
     render(){
         return (
             <div id="ending">
                 <h1>Félicitation, vous avez terminé le parcours</h1>
                 <h2>Merci d'avoir joué !</h2>
-                <button onClick={this.reloadGame}>Retourner à l'accueil</button>
+                <button onClick={this.closeEnding}>Retourner à la carte</button>
+                <button onClick={this.returnToMenu}>Retourner à l'accueil</button>
                 <footer>Cette application a été réalisée par 3 étudiants en informatique dans le cadre du projet de licence 3, sous la tutelle de <a href="https://www.femto-st.fr/fr/personnel-femto/fdadeau">Frédéric Dadeau</a>, maître de conférence à l'Université de Franche-Comté.</footer>
             </div>
         );
